e16f21ed7a1c89bd350d58790b0146d9cad65b7a
MapTech-stack <learntech406@gmial.com>
2025-05-30 11:03:52 +0100

    i added the testimonial section

--- script.js
+++ script.js
@@ -18,3 +18,60 @@ document.addEventListener("click", (e) => {
     menuIcon.style.backgroundImage = menuImg;
   }
 });
+
+const container = document.getElementById("carousel");
+const testimonials = container.querySelectorAll(".testimonial");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+
+let index = 0;
+let intervalId;
+
+function showSlide(i) {
+  index = (i + testimonials.length) % testimonials.length;
+  container.style.transform = `translateX(-${index * 105}%)`;
+}
+
+function startAutoplay() {
+  intervalId = setInterval(() => {
+    showSlide(index + 1);
+  }, 5000); // 5 seconds
+}
+
+function stopAutoplay() {
+  clearInterval(intervalId);
+}
+
+function resetAutoplay() {
+  stopAutoplay();
+  startAutoplay();
+}
+
+prevBtn.addEventListener("click", () => {
+  showSlide(index - 1);
+  resetAutoplay();
+});
+
+nextBtn.addEventListener("click", () => {
+  showSlide(index + 1);
+  resetAutoplay();
+});
+
+let startX = 0;
+
+container.addEventListener("touchstart", (e) => {
+  startX = e.touches[0].clientX;
+  stopAutoplay();
+});
+
+container.addEventListener("touchend", (e) => {
+  const endX = e.changedTouches[0].clientX;
+  const diff = startX - endX;
+
+  if (diff > 50) showSlide(index + 1);
+  else if (diff < -50) showSlide(index - 1);
+
+  resetAutoplay();
+});
+
+startAutoplay();
